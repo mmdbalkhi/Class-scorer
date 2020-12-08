fba7eeec9d77712b293d70c7db45337674002e5a
Komeil Parseh <65954744+KomeilParseh@users.noreply.github.com>
2020-12-08 13:55:20 +0330

    Add files via upload

---
+++ calss.py
@@ -0,0 +1,32 @@
+import json
+
+def rotbe(student_name, student_socore):
+            
+    if student_socore <= 100 and student_socore >= 90:
+        grade = "A"
+        data = [student_name, grade]
+        
+    elif student_socore <= 89 and student_socore >= 80:
+        grade = "B"
+        data = [student_name, grade]
+    elif student_socore <= 79 and student_socore >= 70:
+        grade = "C"
+        data = [student_name, grade]
+    elif student_socore <= 69 and student_socore >= 60:
+        grade = "D"
+        data = [student_name, grade]
+    elif student_socore <= 59 and student_socore >= 0:
+        grade = "F"
+        data = [student_name, grade]
+    else:
+        print("Nomere qhalate!")
+
+    print(data)
+
+datas = []
+with open('test.json') as json_file:
+    data = json.load(json_file)
+    for p in data['people']:
+        name = p['name']
+        socore = p['socore']
+        rotbe(name, socore)
